3acea3e1bfc4f6adc78172a5b3d8d8b599d2cc9c
Gongxl <Gongxl@resnet-5-34.resnet.ucsb.edu>
2015-02-28 01:17:39 -0800

    fix small bugs

--- java/com/ece251/gongxl/transcanner/ScanResult.java
+++ java/com/ece251/gongxl/transcanner/ScanResult.java
@@ -3,25 +3,39 @@ package com.ece251.gongxl.transcanner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.logging.LogRecord;
 
 
 public class ScanResult extends Activity {
-
+        Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
         //set textview context
-        final TextView textView = (TextView)findViewById(R.id.scan_result);
+
+        final EditText editText = (EditText)findViewById(R.id.scan_result);
         Intent intent = getIntent();
-        String ocr_result = intent.getStringExtra("OCR");
-        textView.setText(ocr_result);
+        final String ocr_result = intent.getStringExtra("OCR");
+        editText.setText(ocr_result);
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+
+                editText.setText((String) msg.obj);
+            }
+        };
         final String trans="Fifty shades of Grey";
         ImageButton b2 = (ImageButton)findViewById(R.id.translate);
         b2.setOnClickListener(new Button.OnClickListener() {
@@ -29,12 +43,21 @@ public class ScanResult extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                try {
-                    
-                    textView.setText(Translator.translate(trans));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            try {
+                                String translated = Translator.translate(editText.getText().toString());
+                                Message message = Message.obtain();
+                                message.obj = translated;
+                                handler.sendMessage(message);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
             }
         });
     }
